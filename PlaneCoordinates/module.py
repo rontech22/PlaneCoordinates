2157020b59e24fcc0992df889ea0297cc3e5709d
rinux <ronaldrodelotech@gmail.com>
2025-02-08 23:41:44 -0500

    fixed import module issue

---
+++ PlaneCoordinates/module.py
@@ -0,0 +1,27 @@
+##Creating a class for coordinates in the plane.
+
+class coordinate(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def distance(self, other): #calculate distance between to points in the plane
+        x_diff_sq = (self.x-other.x)**2
+        y_diff_sq = (self.y-other.y)**2
+        return (x_diff_sq+y_diff_sq)**0.5
+    
+    def module(self): #calculate the module of a 2D vector
+        return ((self.x**2)+(self.y**2))**0.5
+    
+    def __str__(self): #print the coordinates as <x,y>
+        return '<'+str(self.x)+','+str(self.y)+'>'
+    
+    def __add__(self, other): #method to add two coordinates
+        i = self.x+other.x
+        j = self.y+other.y
+        return coordinate(i, j)
+    
+    def __sub__(self, other): #method to substract two coordinates
+        i = self.x-other.x
+        j = self.y-other.y
+        return coordinate(i, j)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="PlaneCoordinates",                           # Nombre del paquete
-    version="0.1.0",                                # Versión inicial
+    version="0.1.1",                                # Versión inicial
     packages=find_packages(),                       # Paquetes a incluir
     description="Create 2D Coordinate Object types and perform some operations",  # Breve descripción
     author="Ronald Rodelo",                         # Tu nombre
