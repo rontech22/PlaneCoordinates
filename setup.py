f6a688328aee451fd7ae0c4510cb0230cecc43f3
rinux <ronaldrodelotech@gmail.com>
2025-02-08 22:57:24 -0500

    first release

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PlaneCoordinates",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Create 2D Coordinate Object types and perform some operations",  # Breve descripción
+    author="Ronald Rodelo",                         # Tu nombre
+    author_email="ronaldrodelotech@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/rontech22/PlaneCoordinates",     # URL del proyecto
+)
